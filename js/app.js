19f95bf83299c26359638f8ae24894f1939e140d
A-zeazea <beeisbuzz@naver.com>
2024-04-29 12:49:41 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/**
+ * @param {NodeList} elmts - selected elements from HTML to ad events
+ * @param {string} event - event type e.g 'click', 'mouseenter'...
+ * @param {function} callback - callback function to be executed when event is triggered
+ */
+
+// elmts(string) : number, overcast를 한번에 받아줄 파라미터
+// event(string) : click, mouseenter를 한번에 받아줄 파라미터
+// callback(function) : function () {overcast.style.fontSize = '30px'; 이걸 받아줄 파라미터
+export function eventOnElmts(elmts, event, callback) {
+  for (const elmt of elmts) {
+    elmt.addEventListener(event, callback);
+  }
+}
