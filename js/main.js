8b9d174f7605557430b3e84c4987c5cdf525724e
A-zeazea <beeisbuzz@naver.com>
2024-05-01 12:50:00 +0900

    0501_v0.7

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ import { eventOnElmts } from './app.js';
 //   number[0].style.color = 'red';
 // }
 // eventOnElmts(number, 'click', changeColor);
+
+const searchView = document.querySelector('[data-search-view]'); // 속성 요소를 선택하는 방법
+// console.log(searchView);
+const searchTogglers = document.querySelectorAll('[data-search-toggler]'); // 화살과 돋보기 아이콘의 속성을 갖게 해서 동시 선택
+
+function toggleSearch() {
+  searchView.classList.toggle('active');
+}
+
+eventOnElmts(searchTogglers, 'click', toggleSearch);
